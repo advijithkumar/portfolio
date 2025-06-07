e4b93ea65da0f71ca08d4069b8b1b2740ef8f489
advijithkumar <advijithkumar@gmail.com>
2025-06-08 01:17:28 +0530

    new change

--- src/layouts/MainLayout.jsx
+++ src/layouts/MainLayout.jsx
@@ -1,6 +1,7 @@
-import Navbar from "../components/Navbar";
-import Body from "../components/Body";
-import Footer from "../components/Footer";
+import Navbar from '../components/Navbar';
+import Body from '../components/Body';
+import Footer from '../components/Footer';
+import '../index.css'; 
 const MainLayout = ()=> {
     return(
         <>
